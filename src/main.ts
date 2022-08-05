5638e703cc9a987c735a3d8ebe24d1a6b20a8a1c
saya_zhang <saya_zhang@jiliguala.com>
2022-08-05 09:46:40 +0800

    feat: clean master

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/route';
-import './styles/style.css'
-
-const app = createApp(App);
-
-app.use(router).mount('#app');
